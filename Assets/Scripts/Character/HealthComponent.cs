77929b6f9b3388c85eaeb73834a37269f45c97f0
SefaKILIÇ <sefakkilic@gmail.com>
2024-04-20 10:49:57 +0300

    Revert "Merge branch 'main' of https://github.com/sefa1klc/GameJam"
    This reverts commit 2aa74d7d25eaec7cf5127bd70856cc2786435b04, reversing
    changes made to 58c486043dcb29990523cd1951d9baf7f612576b.

--- Assets/Scripts/Character/HealthComponent.cs
+++ Assets/Scripts/Character/HealthComponent.cs
@@ -8,27 +8,20 @@ namespace Character
         [SerializeField]private float maxHealth = 50f;
         private float currentHealth;
 
-        private Animator anim;
-            
-
         private void Awake()
         {
             currentHealth = maxHealth;
-            anim = GetComponent<Animator>();
         }
 
         public void TakeDamage(float damage)
         {
-            
             currentHealth -= damage;
-            anim.SetTrigger("takingDamage");
             
             Debug.Log(currentHealth);
             
             if (currentHealth <= 0)
             {
-                anim.SetTrigger("isDeath");
-                //Destroy(this.gameObject);
+                Destroy(this.gameObject);
             }
         }
     }
